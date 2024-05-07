c1ae0530dad4b6936c628994cfc2cc4c37b33bd6
Ahmet Barbak <145441448+sBeorn@users.noreply.github.com>
2024-05-07 14:50:03 +0300

    Add files via upload
    In this Python code I have implemented the Euclidean Distance Equation.

---
+++ Euclidean Distance.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## ÖKLİD PYTHON UYGULAMASI
+
+import math
+
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+
+def minDistance(points):
+    n = len(points)
+    min_dist = float('inf')
+    for i in range(n):
+        for j in range(i+1, n):
+            dist = euclideanDistance(points[i], points[j])
+            if dist < min_dist:
+                min_dist = dist
+    return min_dist
+
+points = [(15, 18), (24, 59)]
+distance = minDistance(points)
+print("En kısa mesafe:", distance)
+
+
+
+
+
